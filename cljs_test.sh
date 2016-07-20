09828c4ba9766eca9edbb4dd1aa833434f2fb3c7
Artem Yarulin <artem.yarulin@fessguid.com>
2016-07-20 16:21:11 +0300

    RULES/clojure-clojurescript-buck: Tests runs 3 times faster now
    Previously before running tests we were building it first separetly, which took extra time (around ~30s for CLJS and ~8s for CLJ).
    We we running tests two times: For debug and for advanced build profiles which requires one more extra build (~40s)
    We were using :simple for CLJS optimization level which required additional 5s comparing to simplest :whitespace
    
    Now we have native speed compared to simple lein, no extra work is done.
    
    Eventually we should create `clj*-release` task which would be running test with advanced optimization but only for end project and not for each of the libs

--- cljs_test.sh
+++ cljs_test.sh
@@ -7,4 +7,3 @@ tests=`(cd test && find . -type f ! -name runner.cljs | sed -e "s/.cljs//; s/.cl
 echo "(ns test.runner (:require [doo.runner :refer-macros [doo-all-tests]] \
                                  $tests)) (doo-all-tests)" > test/runner.cljs
 LEIN_ROOT=1 lein doo phantom debug once
-LEIN_ROOT=1 lein doo phantom integration once
